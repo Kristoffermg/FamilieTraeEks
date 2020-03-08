33a80231c33e95d862154d264cda272a86e354be
Kristoffermg <33570390+Kristoffermg@users.noreply.github.com>
2020-03-08 17:47:30 +0100

    Database og funktioner klargjort
    - Database sat op
    - Nogle funktioner klargjort, som skal bruges til genereringen af en person (Ikke alle er klargjort endnu)

--- FamilieTraeProgrammeringEksamen/FamilieTraeProgrammeringEksamen/Form1.Designer.cs
+++ FamilieTraeProgrammeringEksamen/FamilieTraeProgrammeringEksamen/Form1.Designer.cs
@@ -23,38 +23,80 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
-            this.connectionStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numberOfParentGenerations = new System.Windows.Forms.NumericUpDown();
+            this.CreateFamily = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfParentGenerations)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(340, 194);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Connect";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Parent Generations (1-5)";
             // 
-            // connectionStatus
+            // panel1
             // 
-            this.connectionStatus.AutoSize = true;
-            this.connectionStatus.Location = new System.Drawing.Point(337, 163);
-            this.connectionStatus.Name = "connectionStatus";
-            this.connectionStatus.Size = new System.Drawing.Size(78, 13);
-            this.connectionStatus.TabIndex = 1;
-            this.connectionStatus.Text = "Not connected";
+            this.panel1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(799, 46);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(317, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Family Tree Application";
+            // 
+            // numberOfParentGenerations
+            // 
+            this.numberOfParentGenerations.Location = new System.Drawing.Point(40, 81);
+            this.numberOfParentGenerations.Name = "numberOfParentGenerations";
+            this.numberOfParentGenerations.Size = new System.Drawing.Size(120, 20);
+            this.numberOfParentGenerations.TabIndex = 2;
+            this.numberOfParentGenerations.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // CreateFamily
+            // 
+            this.CreateFamily.Location = new System.Drawing.Point(60, 108);
+            this.CreateFamily.Name = "CreateFamily";
+            this.CreateFamily.Size = new System.Drawing.Size(75, 23);
+            this.CreateFamily.TabIndex = 3;
+            this.CreateFamily.Text = "Create Family";
+            this.CreateFamily.UseVisualStyleBackColor = true;
+            this.CreateFamily.Click += new System.EventHandler(this.CreateFamily_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.connectionStatus);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CreateFamily);
+            this.Controls.Add(this.numberOfParentGenerations);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfParentGenerations)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -62,8 +104,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label connectionStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numberOfParentGenerations;
+        private System.Windows.Forms.Button CreateFamily;
     }
 }
 
--- FamilieTraeProgrammeringEksamen/FamilieTraeProgrammeringEksamen/Form1.cs
+++ FamilieTraeProgrammeringEksamen/FamilieTraeProgrammeringEksamen/Form1.cs
@@ -1,26 +1,102 @@
 ﻿using System;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace FamilieTraeProgrammeringEksamen {
     public partial class Form1 : Form {
+           SqlConnection con = new SqlConnection("Data Source=192.168.0.22,49170;Initial Catalog=FamilyTree;Persist Security Info=true;User ID=sa;password=eksamen;");
+        SqlCommand cmd;
         public Form1() {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e) {
-            string connString = "Server=192.168.0.22,49170; Database=FamilyTree; UID=sa; PASSWORD=eksamen;";
+        private void CreateFamily_Click(object sender, EventArgs e) {
             try {
-                MySqlConnection conn = new MySqlConnection();
-                conn.ConnectionString = connString;
-                conn.Open();
-                connectionStatus.Text = "Connected to database!";
+                con.Open();
+                GenerateFamilyMembers();
             }
-            catch (Exception ex) {
-                MessageBox.Show(ex.ToString());
+            catch (Exception error) {
+                MessageBox.Show(error.ToString());
             }
-                
         }
+
+        void GenerateFamilyMembers() {
+            int parentGenerations = Convert.ToInt32(numberOfParentGenerations);
+            int count;
+            string firstName = "";
+            string middleName = "";
+            string lastName = "";
+            int Age;
+            int dayBorn;
+            int monthBorn;
+            int yearBorn;
+            int dayDeath;
+            int monthDeath;
+            int yearDeath;
+            for (int ID = 0; ID < parentGenerations; ID++) {
+                dayBorn = 1;
+                monthBorn = 1;
+                yearBorn = yearBornCalc();
+                dayDeath = 1;
+                monthDeath = 1;
+                yearDeath = yearDeathCalc(yearBorn);
+                Age = dateBorn_dateDeath_ToAge(yearBorn, yearDeath);
+                count = 0;
+                foreach (string nameOutput in yearBornBasedNameGenerator(yearBorn)) {
+                    switch (count) {
+                        case 0:
+                            firstName = nameOutput;
+                            break;
+                        case 1:
+                            middleName = nameOutput;
+                            break;
+                        case 2:
+                            lastName = nameOutput;
+                            break;
+                    }
+                    count++;
+                }
+                cmd = new SqlCommand("insert into Members values('" + ID + "','" + firstName + "','" + middleName + "','" + lastName + "','" + Age + "','" + dayBorn + "','" + monthBorn + "','" + yearBorn + "','" + dayDeath + "','" + monthDeath + "','" + yearDeath + "')", con);
+                cmd.ExecuteNonQuery();
+            }
+            con.Close();
+            // Kald på metoder, som danner kontaktinformationer og familierelationer mellem ID'er
+        }
+
+        IEnumerable<string> yearBornBasedNameGenerator(int yearBorn) {
+            string firstName = "";
+            yield return firstName;
+            string middleName = "";
+            yield return middleName;
+            string lastName = "";
+            yield return lastName;
+        }
+
+        int yearBornCalc() {
+
+            return 1;
+        }
+
+        int yearDeathCalc(int yearBorn) {
+
+            return 0; // Hvis person ikke er død
+        }
+
+        int dateBorn_dateDeath_ToAge(int yearBorn, int yearDeath) {
+            return yearDeath - yearBorn;
+        }
+
+        int phoneNumberGenerator() {
+
+            return 1;
+        }
+
+        string addressGenerator() {
+
+            return "";
+        }
+
+
     }
 }
